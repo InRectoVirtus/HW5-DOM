c9fd445af605c84841f174d834c88ef3ea5483ab
InRectoVirtus <inrectovirtus.8.32@gmail.com>
2021-11-10 18:08:15 +0200

    Add - new

--- HW5-DOM.js
+++ HW5-DOM.js
@@ -2,7 +2,7 @@ document.getElementById("but").addEventListener("click", () => {
         let user = document.getElementById("user").value
         let pass = document.getElementById("pass").value
 
-        document.getElementById("userpass").innerHTML = `Логин - ${user}, пароль - ${pass}`;
+        document.getElementById("userPass").innerHTML = `Логин - ${user}, пароль - ${pass}`;
 
         localStorage.setItem('Логин', user);
 
