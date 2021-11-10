05e9d1070255dcf064160605caac39f2e864d5d3
InRectoVirtus <inrectovirtus.8.32@gmail.com>
2021-11-10 16:59:22 +0200

    add - homeWork

---
+++ HW5-DOM.js
@@ -0,0 +1,16 @@
+function submitForm() {
+    let user = document.getElementById('user').value;
+    let password = document.getElementById('pass').value;
+
+    localStorage.setItem('user', user);
+    localStorage.setItem('password', password);
+
+    showCredentials(user, password);
+}
+
+const showCredentials = (user, password) => {
+
+    let showUserPass = document.getElementById('userpass')
+
+    showUserPass.innerHTML = `Мыло ${user}, Пароль ${password}`
+}
